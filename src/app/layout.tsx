4d62084e4fc0f6993cdb2cc1f187256fc84c39db
rupesh <rpiwal02@gmail.com>
2025-04-04 15:27:03 +0530

    chores: create budget route ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1A1A1A]`}
-      >
-    {children}
+      <body className={`${inter.className} antialiased bg-[#1A1A1A]`}>
+        {children}
       </body>
     </html>
   );

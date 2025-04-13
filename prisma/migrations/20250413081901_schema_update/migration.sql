74c345408600623a7ed8c74ed15c73654c93b934
rupesh <rpiwal02@gmail.com>
2025-04-13 13:50:44 +0530

    chores: prisma migrations

---
+++ prisma/migrations/20250413081901_schema_update/migration.sql
@@ -0,0 +1,162 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Authenticator` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Budget` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Expense` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateEnum
+CREATE TYPE "AccountType" AS ENUM ('CURRENT', 'SAVINGS');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'COMPLETED', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "RecurringInterval" AS ENUM ('DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY');
+
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Authenticator" DROP CONSTRAINT "Authenticator_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Budget" DROP CONSTRAINT "Budget_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Budget" DROP CONSTRAINT "Budget_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Expense" DROP CONSTRAINT "Expense_budgetId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Expense" DROP CONSTRAINT "Expense_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Expense" DROP CONSTRAINT "Expense_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Authenticator";
+
+-- DropTable
+DROP TABLE "Budget";
+
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "Expense";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "AccountType" NOT NULL,
+    "balance" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "description" TEXT,
+    "date" TIMESTAMP(3) NOT NULL,
+    "category" TEXT NOT NULL,
+    "receiptUrl" TEXT,
+    "isRecurring" BOOLEAN NOT NULL DEFAULT false,
+    "recurringInterval" "RecurringInterval",
+    "nextRecurringDate" TIMESTAMP(3),
+    "lastProcessed" TIMESTAMP(3),
+    "status" "TransactionStatus" NOT NULL DEFAULT 'COMPLETED',
+    "userId" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "budgets" (
+    "id" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "lastAlertSent" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "budgets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_clerkUserId_key" ON "users"("clerkUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "accounts_userId_idx" ON "accounts"("userId");
+
+-- CreateIndex
+CREATE INDEX "transactions_userId_idx" ON "transactions"("userId");
+
+-- CreateIndex
+CREATE INDEX "transactions_accountId_idx" ON "transactions"("accountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "budgets_userId_key" ON "budgets"("userId");
+
+-- CreateIndex
+CREATE INDEX "budgets_userId_idx" ON "budgets"("userId");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "budgets" ADD CONSTRAINT "budgets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;

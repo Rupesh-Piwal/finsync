dcec3f779d27bdd3fbe8f0c837a1f8a5068993d6
rupesh <rpiwal02@gmail.com>
2025-06-13 18:59:01 +0530

    fix: prisma output dir

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -14,6 +13,7 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: "5mb",
     },
+    allowedDevOrigins: ["http://localhost:3000"],
   },
 };
 
